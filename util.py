cde7bea7991961f50cabb92a4ea360f61cd943fb
Kevin Vosburgh <kvosbur@purdue.edu>
2020-01-30 16:05:22 -0500

    changed combine to add a gradient of average pixel in between photos. Doesn't look fantastic....

--- util.py
+++ util.py
@@ -1,7 +1,7 @@
 import os
 import ctypes
 import requests
-from PIL import Image
+from PIL import Image, ImageDraw
 
 base_directory = os.path.dirname(os.path.abspath(__file__))
 
@@ -56,3 +56,39 @@ def get_file_from_url(dest_directory, dest_file_name,  file_url, check_correct_a
             return "", "", image_size
     else:
         return file_path, final_path, image_size
+
+
+RIGHT = 0
+LEFT = 1
+
+
+def average_pixel_color(image_object, side):
+    r = 0
+    g = 0
+    b = 0
+    width, height = image_object.size
+    w = 0
+    if side == RIGHT:
+        w = width - 1
+    for h in range(height):
+        nextR, nextG, nextB = image_object.getpixel((w, h))
+        r += nextR
+        g += nextG
+        b += nextB
+
+    return round(r / height), round(g / height), round(b / height)
+
+
+def do_gradient(image_obj, upperLeft, lowerRight, leftColor, rightColor):
+    drawer = ImageDraw.Draw(image_obj)
+    distance_x = lowerRight[0] - upperLeft[0]
+    diff_r = leftColor[0] - rightColor[0]
+    diff_g = leftColor[1] - rightColor[1]
+    diff_b = leftColor[2] - rightColor[2]
+    for x_diff in range(distance_x):
+        x = upperLeft[0] + x_diff
+        percent_dist = x_diff / distance_x
+        colorR = round(leftColor[0] - (percent_dist * diff_r))
+        colorG = round(leftColor[1] - (percent_dist * diff_g))
+        colorB = round(leftColor[2] - (percent_dist * diff_b))
+        drawer.line((x, upperLeft[1], x, lowerRight[1]), fill=(colorR, colorG, colorB))
